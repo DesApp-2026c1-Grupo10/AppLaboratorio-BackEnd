5d3227ea33ae0ae5649b72dc0f7aa3ca1ca6ff97
PreziosoUNAHUR <franco.prezioso@estudiantes.unahur.edu.ar>
2026-05-15 11:08:39 -0300

    Integración de lógica de reservas y controladores

---
+++ db/migrations/20260515000603-create-reagent.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Reagents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      stock: {
+        type: Sequelize.INTEGER,
+      },
+      prep_time: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Reagents');
+  },
+};
