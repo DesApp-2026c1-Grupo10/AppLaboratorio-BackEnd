a2d848b558653a5e421bddab829372cf7fb965bf
NahuelNC1 <nahuel.negreticarballo@estudiantes.unahur.edu.ar>
2026-05-20 22:38:04 -0300

    Cambios en Inventario: controller, models y seeders

--- db/migrations/20260515000553-create-equipment.js
+++ db/migrations/20260515000553-create-equipment.js
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Equipment');
+    await queryInterface.dropTable('Equipments'); //antes "Equipment"
   },
 };
