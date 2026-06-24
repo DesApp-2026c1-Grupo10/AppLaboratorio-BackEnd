af117d229c163253ff79c28ec20836e5a3e85d51
PreziosoUNAHUR <franco.prezioso@estudiantes.unahur.edu.ar>
2026-06-24 12:25:39 -0300

    Seeder actualizado: passwords con bcrypt hash + usuario de testing

--- db/seeders/20201216173018-usuarios-proceres.js
+++ db/seeders/20201216173018-usuarios-proceres.js
@@ -1,13 +1,16 @@
 'use strict';
 
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const hash = bcrypt.hashSync('123456', 10);
     await queryInterface.bulkInsert('Usuarios', [
       {
         nombre: 'Juana',
         apellido: 'Azurduy',
         email: 'juana.azurduy@universidad.edu',
-        password: '123456',
+        password: hash,
         rol: 'Profesor',
         fechaNacimiento: '1780-07-12',
         avatarUrl: 'http://www.laizquierdadiario.com/IMG/arton21559.jpg',
@@ -18,7 +21,7 @@ module.exports = {
         nombre: 'José',
         apellido: 'Artigas',
         email: 'jose.artigas@universidad.edu',
-        password: '123456',
+        password: hash,
         rol: 'Profesor',
         fechaNacimiento: '1764-06-19',
         avatarUrl:
@@ -30,11 +33,22 @@ module.exports = {
         nombre: 'Simón',
         apellido: 'Bolívar',
         email: 'simon.bolivar@universidad.edu',
-        password: '123456',
+        password: hash,
         rol: 'Profesor',
         fechaNacimiento: '1783-04-24',
         avatarUrl:
           'https://img.goraymi.com/2019/01/15/95f0f23f742a6f7a28fd225745095d04_lg.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nombre: 'Desarrollador',
+        apellido: 'Test',
+        email: 'desarrollador@test.com',
+        password: bcrypt.hashSync('123', 10),
+        rol: 'Profesor',
+        fechaNacimiento: '1990-01-01',
+        avatarUrl: null,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
